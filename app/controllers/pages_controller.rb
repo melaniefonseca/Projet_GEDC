b6deb4f4414cece20608ffb49104dfe5455bfc88
qujollivet <quentin.jollivet-castelot@etu.u-bordeaux.fr>
2021-05-11 14:37:17 +0200

    Ajout du header et du footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,37 @@ class PagesController < ApplicationController
   def home
 
   end
+
+  def showForm
+    @json_request = '{
+      "annee": "",
+      "nom": "",
+      "entreprise": "",
+      "poste": "",
+      "activité": "",
+      "date":"",
+      "commentaire": "",
+      "data": [{
+          "titre": "Savoir-être",
+          "choix": ["Non évalué", "A travailler", "Acquis"],
+          "competences": [{
+              "intitule": "Investi et motivé",
+              "requis": 2
+            }, {
+              "intitule": "Anglais",
+              "requis": 1
+            }
+          ]
+        }, {
+          "titre": "Compétences transverses",
+          "choix": ["Non évalué", "Avec aide", "Autonome", "Niveau professionnel"],
+          "competences": [ {
+              "intitule": "Organisé",
+              "requis": 3
+            }
+          ]
+        }
+      ]
+    }'
+  end
 end
