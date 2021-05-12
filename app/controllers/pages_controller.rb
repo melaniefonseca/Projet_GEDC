084960a4d6d5438271eae635a1029ddb2c012c8a
magdaleinesoury <soury.magdaleine@outlook.fr>
2021-05-12 13:58:04 +0200

    creation page visualisation etudiant + details page remplissage etudiant

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,36 +6,18 @@ class PagesController < ApplicationController
   def showForm
 
     require 'json'
-    text =  '{
-      "annee": "",
-      "nom": "",
-      "entreprise": "",
-      "poste": "",
-      "activité": "",
-      "date":"",
-      "commentaire": "",
-      "data": [{
-          "titre": "Savoir-être",
-          "choix": ["Non évalué", "A travailler", "Acquis"],
-          "competences": [{
-              "intitule": "Investi et motivé",
-              "requis": 2
-            }, {
-              "intitule": "Anglais",
-              "requis": 1
-            }
-          ]
-        }, {
-          "titre": "Compétences transverses",
-          "choix": ["Non évalué", "Avec aide", "Autonome", "Niveau professionnel"],
-          "competences": [ {
-              "intitule": "Organisé",
-              "requis": 3
-            }
-          ]
-        }
-      ]
-    }'
+    text =  '{"annee": "","nom": "","entreprise": "","poste": "","activité": "","date":"","sections":[{"titre": "Savoir-être","choix": ["Non évalué", "A travailler", "Acquis"],"competences":[{"intitule": "Investi et motivé","requis": 2,"selection": 2},{"intitule": "Anglais","requis": 1,"selection": 2}]},{"titre": "Compétences transverses","choix": ["Non évalué", "Avec aide", "Autonome", "Niveau professionnel"],"competences":[{"intitule": "Organisé","requis": 3,"selection": 1}]}],"commentaire": ""}'
+
+    @data = JSON.parse(text)  # <--- no `to_json`
+    # => {"one"=>1, "two"=>2}
+    puts @data.class
+
+  end
+
+  def viewAutoEval
+
+    require 'json'
+    text =  '{"année": "2020-2021","nom": "Dumas Richard","entreprise": "SAS Entreprise","poste": "Développeur web","activité": "Développement de modules pour ERP web","date":"26/02/2021","sections":[{"titre": "Savoir-être","choix": ["Non évalué", "A travailler", "Acquis"],"competences":[{"intitule": "Investi et motivé","requis": 2,"selection": 2},{"intitule": "Anglais","requis": 1,"selection": 2}]},{"titre": "Compétences transverses","choix": ["Non évalué", "Avec aide", "Autonome", "Niveau professionnel"],"competences":[{"intitule": "Organisé","requis": 3,"selection": 1}]}],"commentaire": "Test de commentaire Test de commentaire Test de commentaire Test de commentaire"}'
 
     @data = JSON.parse(text)  # <--- no `to_json`
     # => {"one"=>1, "two"=>2}
