60c9af676bc17e506dd327a1dd165f5192aa5e4f
Icoops <tristan.celor@gmail.com>
2021-05-20 09:49:50 +0200

    Ajout page d'edition

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
   get '/', to: 'pages#home'
   get '/evaluation', to: 'pages#evaluation'
   post '/evaluation/save', to: 'pages#save'
@@ -7,4 +8,6 @@ Rails.application.routes.draw do
   get '/tableaudebord', to: 'pages#tableauDeBord'
   get '/menuetudiant', to: 'pages#menuEtudiant'
   get '/menurespstage', to: 'pages#menuRespStage'
+
+  resources :ge_formats
 end
